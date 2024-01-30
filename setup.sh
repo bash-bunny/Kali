afd23bd01f68cf4d409d69f6750a1ef86b6221b1
DeadS3c <52109756+DeadS3c@users.noreply.github.com>
2024-01-30 14:52:54 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Setup configuration files for Kali Linux
+
+# Install all the programs need it
+sudo apt install -y tmux vim lynx
+
+# Cp files to their location
+cp {.bashrc,.inputrc,.vimrc,.tmux.conf} ~/
+
+## Create the folders if it doesn't exists
+if [[ -f ~/.config/lynx ]];then
+	mkdir -p ~/.config/lynx
+fi
+
+if [[ -f ~/.local/bin ]]; then
+	mkdir -p ~/.local/bin
+fi
+
+## Cp the rest of the files to their location
+cp lynx.* ~/.config/lynx/
+cp {lynx,bing,duck,google,vic,ix,urlencode,path} ~/.local/bin/
+
+# Change shell to bash
+chsh -s /usr/bin/bash
